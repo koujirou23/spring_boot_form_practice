072b74db36ab34c8b9f969449fbf0f576b0217ac
koujirou23 <70497233+koujirou23@users.noreply.github.com>
2021-04-07 22:42:17 +0900

    エラーメッセージ等設定 form の調整

--- MyfirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ MyfirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -2,7 +2,10 @@ package com.example.demo.app.inquiry;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +13,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InquiryController {
 
  	@GetMapping("/form")
-	public String form(Model model) {
+ 	// inquiryForm 入力した値やバリデーションエラーメッセージを取得
+	public String form(InquiryForm inquiryForm, Model model) {      // フォームクラスに引数を加えるとフォームの入力欄と紐付けられて自動で初期化してくれる
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/form";
+	}
+ 	
+ 	// 戻るボタンを押したときの画面遷移
+ 	@PostMapping("/form")
+	public String formGoBack(InquiryForm inquiryForm, Model model) {      
+		model.addAttribute("title", "Inquiry Form");
+		return "inquiry/form";
+	}
+ 	
+ 	// Postでなにかデータを送信したときのみアクセスするページ
+ 	@PostMapping("/confirm")
+ 	// @validated inquiryFormにバリデーションがかかる
+ 	// @BindingResult バリデーションの結果が入る
+ 	public String confirm(@Validated InquiryForm inquiryForm, BindingResult result, Model model) {
+ 		// バリデーションエラーが有ったときの処理
+ 		if(result.hasErrors()) {
+ 			model.addAttribute("title", "Inquiry Form");
+ 		return "inquiry/form";
+ 		}
+ 		// エラーが無かった場合
+ 		model.addAttribute("title", "Confirm Page");
+ 		return "inquiry/confirm";
 	}
 }
